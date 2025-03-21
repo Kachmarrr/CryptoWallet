ec037ef145e3aa22d0c9c5d0d5e42f791ed6805c
Andriy  Kachmar <andriykachmar@Iryna-Kinash-Iryna-Kinash.local>
2025-03-21 16:15:42 +0200

    back to past version

--- src/main/java/com/ak/CryptoWallet/entity/Account.java
+++ src/main/java/com/ak/CryptoWallet/entity/Account.java
@@ -1,12 +1,15 @@
 package com.ak.CryptoWallet.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//@Getter
-//@Setter
+@Getter
+@Setter
 public class Account {
 
     private String id;
@@ -15,37 +18,5 @@ public class Account {
 
     //private Map<String, Transaction> transactions = new HashMap<>();
     private List<Transaction> transactions = new ArrayList<Transaction>();
-
-    public List<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(List<Transaction> transactions) {
-        this.transactions = transactions;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
 
 }
